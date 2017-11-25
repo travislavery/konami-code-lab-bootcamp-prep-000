d7867719d943a30eeec36ee6c0a71ea5bd14fa03
Travis Lavery <travisalavery@gmail.com>
2017-11-25 01:56:08 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,5 +4,14 @@ function init() {
   // Write your JavaScript code inside the init() function
   document.body.addEventListener('keydown', function(event){
     const key = parseInt(event.detail || event.key)
+    if (key === code[index]) {
+      index++;
+      if (index === code.length) {
+        alert("success");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
   })
 }
